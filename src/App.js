e9deceec574f03a1bd67b4a81294002dc20a3e72
gokhan-kaplan <gokhan.kaplan519@outlook.com>
2023-11-08 14:31:47 +0300

    S6G3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [apodData, setApodData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+        );
+        setApodData(response.data);
+      } catch (error) {
+        console.error("Error fetching APOD data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <header className="App-header">
+        <h1>NASA Astronomy Picture of the Day</h1>
+        {apodData && (
+          <>
+            <img src={apodData.url} alt={apodData.title} />
+            <h2>{apodData.title}</h2>
+            <p>{apodData.explanation}</p>
+          </>
+        )}
+      </header>
     </div>
   );
 }
